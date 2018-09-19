d296a0a4313cbae2e8ae586da98a8ac58d93614f
Omer Aloni <omeraloni@gmail.com>
2018-09-19 15:13:09 +0300

    Generes API ready

--- index.js
+++ index.js
@@ -5,10 +5,77 @@ const app = express();
 app.use(express.json());
 
 const genres = [
-    { id: 1, genre: 'action' },
-    { id: 2, genre: 'drama' },
-    { id: 3, genre: 'comedy' },
-    { id: 4, genre: 'horror' },
-    { id: 5, genre: 'documentary' },
+    { id: 1, name: 'action' },
+    { id: 2, name: 'drama' },
+    { id: 3, name: 'comedy' },
+    { id: 4, name: 'horror' },
+    { id: 5, name: 'documentary' },
 ];
 
+function validateGenre(genres) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(genres, schema);
+}
+
+function findGenre(id) {
+    return genres.find(c => c.id === id);
+}
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+})
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = findGenre(parseInt(req.params.id));
+    
+    if (!genre) return res.status(404).send(`Genre ${req.params.id} not found`);
+    
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Create new genres
+    const genre = { 
+        id: genres.length + 1,
+        name: req.body.name,
+    };
+
+    genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = findGenre(parseInt(req.params.id));
+
+    if (!genre) return res.status(404).send(`Genre ${req.params.id} not found`);
+
+    const { error } = validateGenre(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = findGenre(parseInt(req.params.id));
+
+    if (!genre) return res.status(404).send(`Genre ${req.params.id} not found`);
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    res.send(genre);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});

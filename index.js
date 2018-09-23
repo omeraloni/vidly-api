1c21bc4a88b1ebcd6f3c367e3dd782a1cd44cc27
Omer Aloni <omeraloni@gmail.com>
2018-09-23 09:02:43 +0300

    Added config and debug modules

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const config = require('config');
+const debugStartup = require('debug')('app:startup');
+const debugDb = require('debug')('app:db');
+const debugConfig = require('debug')('app:config');
 const genres = require('./routes/genres');
 const app = express();
 
 app.use(express.json());
 app.use('/api/genres', genres);
+
+// Configuration
+debugConfig(`App Name: ${config.get('name')}`);
+
+const debugLevel = config.get('debug_level');
+debugStartup(`Debug level: ${debugLevel}`);
+
+if (app.get('env') == 'development') {
+
+    if (debugLevel > 0) {
+        app.use(morgan('tiny'));
+        debugStartup('Morgan enabled');
+    }
+}
 
 const port = process.env.PORT || 4000;
 

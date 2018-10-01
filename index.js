e4613b45a8e81834147b33a41b9c94db4489a3d7
Omer Aloni <omeraloni@gmail.com>
2018-10-01 17:16:28 +0300

    Added auth route

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -28,7 +29,8 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
-app.use('/api/users', users)
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 // Configuration
 debugConfig(`App Name: ${config.get('name')}`);

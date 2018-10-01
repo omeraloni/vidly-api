0102f48d1ee0583f69b79c667553ef391de08b5b
Omer Aloni <omeraloni@gmail.com>
2018-10-01 23:25:20 +0300

    Added some comments

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,9 @@
 const { User, validate } = require('../models/user');
 
 module.exports = function (req, res, next) {
+    // 401 Unauthorized
+    // 403 Forbidden
+    
     if (!req.user.isAdmin) return res.status(403).send('Access denied.');
     next();
 }

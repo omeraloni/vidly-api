30a53ea2b44fe61dfb8912448a914a19cfc80962
Omer Aloni <omeraloni@gmail.com>
2018-10-02 23:00:34 +0300

    Improved winston error logging

--- middleware/error.js
+++ middleware/error.js
@@ -1,32 +1,42 @@
 const path = require('path');
 const { createLogger, format, transports } = require('winston');
-const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, label, colorize, printf, prettyPrint } = format;
  
+const LOG_DIR = path.normalize(`${process.cwd()}/logs`);
+const LABEL = 'router';
+
 const myFormat = printf(info => {
   return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
 });
 
-const LOG_DIR = path.normalize(`${process.cwd()}/logs`);
-
 const logger = createLogger({
     level: 'info',
     format: combine(
-        label({ label: 'router' }),
+        label({ label: LABEL }),
         timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        myFormat
+        prettyPrint()
       ),
-      transports: [
-      //
-      // - Write to all logs with level `info` and below to `combined.log` 
-      // - Write all logs error (and below) to `error.log`.
-      //
-      new transports.File({ dirname: LOG_DIR,  filename: 'error.log', level: 'error' }),
-      new transports.File({ dirname: LOG_DIR, filename: 'combined.log' })
+    transports: [
+      new transports.File({ 
+          dirname: LOG_DIR,  
+          filename: 'error.log', 
+          level: 'error' 
+        }),
+      new transports.File({
+          dirname: LOG_DIR, 
+          filename: 'combined.log' })
     ]
   });
 
 if (process.env.NODE_ENV == 'development') {
-    logger.add(new transports.Console());
+    logger.add(new transports.Console({
+        format: combine(
+            colorize(),
+            label({ label: LABEL }),
+            timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            myFormat            
+        )
+    }));
 }
 
 module.exports = function(err, req, res, next) {

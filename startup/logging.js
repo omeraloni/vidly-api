9b2da221d16cb4b1578105fa67cb50c740966f49
Omer Aloni <omeraloni@gmail.com>
2018-10-03 08:28:46 +0300

    Yet again improved logging

--- startup/logging.js
+++ startup/logging.js
@@ -3,13 +3,41 @@ const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
 
-const LOG_DIR = path.normalize(`${process.cwd()}/logs`);
+const LOG_DIR = path.normalize(`${process.cwd()}/logs`); // TODO: config.get('logDir')
+const TIMESTAMP_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+
+// FYI
+const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4,
+    silly: 5
+};
+
+function prettyPrintTimestamped() {
+    winston.format.combine(
+        winston.format.timestamp({ format: TIMESTAMP_FORMAT }),
+        winston.format.prettyPrint()
+    );
+}
+
+function consolePrint() {
+    winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: TIMESTAMP_FORMAT }),
+        winston.format.printf(info => {
+            return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
+          })
+    );
+}
 
 module.exports = function() {
     winston.exceptions.handle(new winston.transports.File({
         dirname: LOG_DIR,
         filename: 'exceptions.log',
-        format: winston.format.prettyPrint(),
+        format: prettyPrintTimestamped(),
         handleExceptions: true
     }));
 
@@ -24,23 +52,19 @@ module.exports = function() {
     winston.add(new winston.transports.File({
         dirname: LOG_DIR,
         filename: 'errors.log',
-        level: 'error'
+        level: 'error',
+        format: prettyPrintTimestamped(),
     }));
 
     winston.add(new winston.transports.File({
         dirname: LOG_DIR,
-        filename: 'combined.log'
+        filename: 'combined.log',
+        format: prettyPrintTimestamped(),
     }));
 
     if (process.env.NODE_ENV == 'development') {
         winston.add(new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                winston.format.printf(info => {
-                    return `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`;
-                  })
-            )
+            format: consolePrint(),
         }));
 
         //winston.exitOnError = false;

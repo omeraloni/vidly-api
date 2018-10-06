da009dd06f7c3266576f8d7c0e02e879fa439c8c
Omer Aloni <omeraloni@gmail.com>
2018-10-07 00:10:35 +0300

    Added helmet and compression to prod

--- routes/returns.js
+++ routes/returns.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
-const Fawn = require('fawn');
 const auth = require('../middleware/auth');
 const validate = require('../middleware/validate');
 const Joi = require('joi');
 
 router.post('/', [auth, validate(validateReturn)], async (req, res) => {
-
     const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
     if (!rental) return res.status(404).send('Invalid ID.');
     if (rental.dateReturned) return res.status(400).send('Return already processed.');
@@ -18,13 +16,6 @@ router.post('/', [auth, validate(validateReturn)], async (req, res) => {
     await Movie.update({ _id: rental.movie._id }, {
         $inc: { numberInStock: 1 }
     });
-
-    /*
-    new Fawn.Task()
-        .save('rentals', rental)
-        .save('movies', movie)
-        .run();
-    */
 
     res.send(rental);
 });
